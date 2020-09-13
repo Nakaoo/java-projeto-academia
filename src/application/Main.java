7809f35b5394a23a6cf2f048fd58704553087bce
Kawan Nakao <nakaokawan@gmail.com>
2020-09-13 18:26:48 -0300

    Criando metodo para abrir uma view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,25 +9,31 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
 			
-			//ajustando menu
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Marombacademia");
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,32 +1,68 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
 	@FXML
-	private MenuItem menuIemCadastrarCli;
+	private MenuItem menuIemCliCadastrar;
 	
 	@FXML
-	private MenuItem menuIemListarCli;
+	private MenuItem menuIemCliListar;
+	
+	@FXML 
+	private MenuItem menuItemAjudaSobre;
 	
 	@FXML
-	public void onMenuItemCadastrarCliAction() {
+	public void onMenuItemCliCadastrarAction() {
 		System.out.println("Cadastrar");
 	}
 	
 	@FXML
-	public void onMenuItemListarCliAction() {
+	public void onMenuItemCliListarAction() {
 		System.out.println("Listar");
 	}
+	
+	@FXML
+	public void onMenuItemAjudaSobreAction() {
+		this.loadView("/gui/About.fxml");
+	}
+	
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IOException", "Erro carregando pagina", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
